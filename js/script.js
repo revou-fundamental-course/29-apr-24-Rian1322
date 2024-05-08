a6cb9f4ae08159cbf9e4e601a97ff8e490f8c04c
Rian1322 <168444400+Rian1322@users.noreply.github.com>
2024-05-08 14:05:31 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function validation() {
+    console.log('mantap mas bro');
+    alert('mantap lah')
+}
+document.getElementById('button-home').addEventListener('click' , () => validation ())
+document.getElementById('button-package').addEventListener('click' , () => validation ())
+document.getElementById('button-call').addEventListener('click' , () => validation ())
