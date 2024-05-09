72f14e5b309fb96f41de9b2e13c9ceb9c53c9c66
Rian1322 <168444400+Rian1322@users.noreply.github.com>
2024-05-09 14:14:45 +0700

    add_js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,51 @@
-function validation() {
-    console.log('mantap mas bro');
-    alert('mantap lah')
+function validationsatu() {
+    console.log('Good');
 }
-document.getElementById('button-home').addEventListener('click' , () => validation ())
-document.getElementById('button-package').addEventListener('click' , () => validation ())
-document.getElementById('button-call').addEventListener('click' , () => validation ())
+document.getElementById('button-home').addEventListener('click' , () => validationsatu ())
+document.getElementById('button-package').addEventListener('click' , () => validationsatu ())
+document.getElementById('button-call').addEventListener('click' , () => validationsatu ())
+
+function validation(){
+    let username = document.getElementById('input-name-left').value;
+    console.log;(username);
+    if(username == "" || username == 'null'){
+        alert ('inputan anda kosong, silahkan cek kembali')
+    }
+    else{
+        alert('welcome '+ username)
+        console.log(username);
+    }
+} 
+
+let indexSlide = 1;
+showSlide(1);
+
+function nextSlide(n){
+    console.log('nextSlide' + n);
+    showSlide(indexSlide += n);
+}
+
+function showSlide(index){
+    let listImage = document.getElementsByClassName('bener-item');
+    console.log(listImage)
+    console.log('banyaknya image:' + listImage.length);
+
+    if(index > listImage.length ) indexSlide = 1;
+    let i = 0;
+    while(i < listImage.length){
+        listImage[i].style.display ='none';      
+        i++
+    }
+    listImage[indexSlide - 1].style.display = 'block'
+    console.log('showSlide' + index);
+}
+setInterval(()=> nextSlide(1), 2000)
+
+document.getElementById('send-btn').addEventListener('click' , () => nextSlide(1))
+
+
+
+
+
+
+
